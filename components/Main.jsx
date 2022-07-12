8a398aac65f799917601525017d90220ff55fd19
Amir Mohammad Hamzavi <83181894+Amir-Hmzv@users.noreply.github.com>
2022-07-12 04:49:19 -0700

    finally

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
             LET`S BUILD SOMETHING TOGETHER
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hi, I`m <span className='text-[#5651e5] '> Client</span>
+            Hi, I`m <span className='text-[#5651e5] '> Amir Mohammad</span>
           </h1>
           <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
